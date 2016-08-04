f601e60fc303cfac79b93b1728b985e8e90b454c
Christian Fritz <christian@savioke.com>
2016-08-04 08:08:43 +0200

    Updated version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'chfritz:serversync',
-  version: '0.2.2',
+  version: '0.3.0',
   summary: 'Synchronize collections across multiple meteor servers (belonging to separate apps)',
   git: 'https://github.com/chfritz/meteor-serversync',
   documentation: 'README.md'
